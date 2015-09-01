732beac877498e6490d608f63550d22d2b761eaa
Christopher Cote <chris@entropealabs.com>
2015-09-01 13:43:41 -0500

    remove logging statement

--- lablog/controllers/node.py
+++ lablog/controllers/node.py
@@ -33,9 +33,6 @@ def get_nodes():
 
 @node.route("/<node_id>/sensors", methods=["POST"])
 def node_sensors(node_id):
-    logging.info(request.data)
-    logging.info(config.SKEY)
-    logging.info(KEY)
     j = aes.decrypt(request.data, KEY)
     logging.info(j)
     j = json.loads(j)
