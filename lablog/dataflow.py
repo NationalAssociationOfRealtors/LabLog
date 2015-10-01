3cbf123668ab709c27b7635ba5846e8da26fd0cc
Christopher Cote <chris@entropealabs.com>
2015-10-01 16:50:18 -0500

    start of dataflow diagram

---
+++ lablog/dataflow.py
@@ -0,0 +1,72 @@
+energy = {
+    "name":"Energy",
+    "nodes":[
+        "Energy Gateway",
+        "Home Energy Monitor",
+        "Universal Power Supply"
+    ],
+    "data":[
+        "Power",
+        "Received",
+        "Delivered",
+        "Price",
+        "Amps",
+        "Voltage",
+        "Current",
+    ]
+}
+
+presence = {
+    "name":"Presence",
+    "nodes":[
+        "Presence"
+    ],
+    "data":[
+        "Presence"
+    ]
+}
+
+air_quality = {
+    "name": "Air Quality",
+    "nodes":[
+        "Sensor Node",
+        "Netatmo Weather Station",
+        "Cube Sensors",
+    ],
+    "data":[
+        "Temperature",
+        "Humidity",
+        "Light",
+        "CO2",
+        "Sound",
+        "VOC"
+    ]
+}
+
+weather = {
+    "name":"Weather",
+    "nodes":[
+        "Wunderground"
+    ],
+    "data":[
+        'UV',
+        'Dew Point',
+        'Feels Like',
+        'Heat Index',
+        'Precipitation',
+        'Pressure'
+        'Humidity',
+        'Solar Radiation',
+        'Temperature',
+        'Visibility',
+        'Wind Direction',
+        'Wind Gust',
+        'Wind Speed',
+        'Wind Chill'
+    ]
+}
+
+triggers = [
+    "Presence",
+    "CO2",
+]
