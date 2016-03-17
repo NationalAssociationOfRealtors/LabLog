4b628d89457cb9f3c64d05af2c7b64cbb7f3a716
Christopher Cote <chris@entropealabs.com>
2016-03-17 16:26:43 -0500

    adds support for optimized json data structure

--- lablog/interfaces/sensornode.py
+++ lablog/interfaces/sensornode.py
@@ -5,6 +5,7 @@ from lablog import config
 from lablog import messages
 from datetime import datetime
 import json
+import logging
 
 k = list(config.SKEY)
 k.append(0x00)
@@ -23,8 +24,10 @@ class Node(Interface):
         return j
 
     def parse_data(self, data):
+        v_map = {'t':'temperature','h':'humidity','l':'light','c':'co2','v':'voc','d':'dust'}
         points = []
         for k,v in data.iteritems():
+            k = v_map.get(k, k)
             if v:
                 points.append(dict(
                     measurement="{}.{}".format(self.measurement_key, k),
